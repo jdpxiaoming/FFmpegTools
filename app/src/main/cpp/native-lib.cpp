cb8f948b227757b16b1251e98bc127e1fe279c22
caixingming <Cxm#2020>
2020-05-11 21:23:33 +0800

    firt commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,23 @@
+#include <jni.h>
+//#include <string>
+
+extern "C"{
+//引用ffmpeg.h头方法.
+//#include "ffmpeg.h"
+#include "libavformat/avformat.h"
+#include "libavcodec/avcodec.h"
+}
+
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_example_ffmpegtools_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+
+//    hello = "Hello from C++";
+    char str[25];
+    sprintf(str, "%d", avcodec_version());
+
+    return env->NewStringUTF(str);
+}
